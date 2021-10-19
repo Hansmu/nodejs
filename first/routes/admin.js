302abd7c08bbdb8186843f40ed2d8654c056952a
hansmu <hans.miikael@gmail.com>
2021-10-19 14:23:51 +0300

    Add Handlebar and Pug template examples

--- first/routes/admin.js
+++ first/routes/admin.js
@@ -1,16 +1,28 @@
-const path = require('path');
-const {rootPath} = require("../utils/path-utils");
 const {Router} = require('express');
 
 const router = Router(); // Router creates like a mini Express app that communicates with the other apps
 
+const products = [];
+
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(rootPath, 'views', 'add-product.html'))
+    res.render('add-product', {
+        pageTitle: 'Add product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 });
 
 router.post('/product', (req, res) => {
     console.log(req.body);
+    products.push({
+        title: req.body.title
+    });
     res.redirect('/admin/add-product');
 });
 
-module.exports = router;
+module.exports = {
+    adminRoutes: router,
+    products
+};
--- first/server.js
+++ first/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const expressHandlebars = require('express-handlebars');
 
-const adminRoutes = require('./routes/admin');
+const {adminRoutes, products} = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 const {rootPath} = require("./utils/path-utils");
 
 const app = express();
 const port = 3030;
+
+const TEMPLATE_EXTENSION = 'hbs';
+
+// The name defines the extension. If it was hbs, then you could use .hbs files
+app.engine(
+    TEMPLATE_EXTENSION,
+    expressHandlebars({
+        layoutsDir: 'views/layouts/', // This would be the default
+        defaultLayout: 'main-layout', // Has to have a main-layout folder in the layouts folder,
+        extname: TEMPLATE_EXTENSION
+    })
+);
+app.set('view engine', TEMPLATE_EXTENSION);
+
+// app.set('view engine', 'pug'); // Pug support is built in, so only need the library. Handlebars, however, does not have default support.
 
 // Middleware to serve static files. It looks at the extensions and decides whether to direct it here or not.
 // You can add multiple static file locations and it'll funnel through all of them until it gets a hit.
@@ -23,7 +39,9 @@ app.use(shopRoutes);
 
 // Add a 404 handler. use handles all requests coming in.
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootPath, 'views', '404.html'));
+    res.status(404).render('404', {
+        pageTitle: 'Not found'
+    });
 });
 
 app.listen(port);
